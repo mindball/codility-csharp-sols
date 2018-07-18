48ed8205755c39a89cbc06af2ed9dab76456280c
mindball <mincho.balaliew@gmail.com>
2018-07-18 10:20:57 +0300

    Lesson 2 Arrays

--- Program.cs
+++ Program.cs
@@ -3,67 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace BinaryGap
+namespace OddOccurrencesInArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int number;
-            int.TryParse(Console.ReadLine(), out number);
-           
-            
-            if (number == 0 || number == 1 || number == 2
-                        || number == 3 || number == 4)
-                Console.WriteLine(0);
-            else
-                Console.WriteLine(BinaryGap(number));
+            int[] array = { 1, 2, 1, 2, 1, 2, 4, 5, 6 };
+            var s1 = Stopwatch.StartNew();
+            Console.WriteLine(Solution1(array));
+            s1.Stop();
+            Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 1000000) /
+            100000000).ToString("0.00 ns"));
+
+            var s2 = Stopwatch.StartNew();
+            Console.WriteLine(Solution2(array));
+            s2.Stop();
+            Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 1000000) /
+            100000000).ToString("0.00 ns"));
         }
 
-        public static int BinaryGap(int number)
+        private static int Solution1(int[] array)
         {
-            int currentCountConZeros = 0;
-            int maxCountConZers = 0;
-            int tempNumber = 0;
-
-            while (number > 0)
+            bool[] passingIndex = new bool[array.Length];
+            for (int i = 0; i < (array.Length - 1); i++)
             {
-                if (number == 1)
+                if (passingIndex[i] != true)
                 {
-                   break;
-                }
-                //looking for  first One (or odd number)
-                else if (number % 2 != 0)
-                {
-                    tempNumber = number / 2;
-
-                    while (tempNumber % 2 == 0)
+                    for (int j = i + 1; j < array.Length; j++)
                     {
-                        currentCountConZeros++;
-                        tempNumber /= 2;
+                        if (passingIndex[j] != true)
+                        {
+                            if (array[i] == array[j])
+                            {
+                                passingIndex[i] = true;
+                                passingIndex[j] = true;
+                                break;
+                            }
+                        }
                     }
-                    if (maxCountConZers < currentCountConZeros)
-                        maxCountConZers = currentCountConZeros;
-                                
-                    number = tempNumber;
-                    currentCountConZeros = 0;
-
-                }                
-                
-                else
-                {
-                    number /= 2;
                 }
-                
-                
 
             }
 
-            return maxCountConZers;
+            int result = 0;
+            for (int i = 0; i < passingIndex.Length; i++)
+            {
+                if(passingIndex[i] == false)
+                {
+                    result = array[i];
+                }
+            }
 
+            return result;
+            
         }
 
-       
+        private static int Solution2(int[] array)
+        {
+            Dictionary<int, int> occurs = new Dictionary<int, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(occurs.ContainsKey(array[i]) && occurs[array[i]] == 1)
+                {
+                    occurs.Remove(array[i]);
+                }
+                else
+                {
+                    occurs.Add(array[i], 1);
+                    
+                }
+            }
+            int result = 0;
+            foreach (var item in occurs)
+            {
+                result = item.Key;
+            }
+            return result;
+        }
+
+        
     }
 }
