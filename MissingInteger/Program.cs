08e98a29aba5a288fbdbaf7e0db9e03a51a88e5e
mindball <mincho.balaliew@gmail.com>
2018-07-20 15:44:44 +0300

    Lesson 4 Counting Elements

---
+++ MissingInteger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sol = Solution(new int[] {1});
+            
+        }
+
+        public static int Solution(int[] A)
+        {
+            int missInt = 1;
+            Array.Sort(A); 
+
+
+           
+
+            for (int i = 0; i < A.Length; i++)
+            {                
+                if(A[i] > 0 && A[i] < 100001)
+                {
+                    if (missInt == A[i])
+                        missInt++;
+                }
+            }
+
+            return missInt;
+           
+           
+        }
+    }
+}
